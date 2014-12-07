3141346182945d9816d2e33865b8c495ae17fc7a
BuildTools <unconfigured@null.spigotmc.org>
2014-12-06 20:46:37 -0500

    Worked a bit on /paidtowork && /ptw

--- src/main/java/me/mrson/PaidtoWork/Commands/CMD_PaidtoWork.java
+++ src/main/java/me/mrson/PaidtoWork/Commands/CMD_PaidtoWork.java
@@ -4,19 +4,30 @@ import me.mrson.PaidtoWork.Base;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
+import org.bukkit.entity.Player;
 
 /**
  * Created by Dominic on 12/4/2014.
  */
 public class CMD_PaidtoWork implements CommandExecutor{
     private final Base base;
-
     public CMD_PaidtoWork(Base base){
         this.base = base;
     }
 
+    private String permission = "ptw.main";
+
     @Override
     public boolean onCommand(CommandSender sender, Command cmd, String label, String[] args) {
+        Player player = (Player) sender;
+        String command = cmd.getName();
+
+        if(command.equalsIgnoreCase("paidtowork" ) || command.equalsIgnoreCase("ptw")){
+            if(player.hasPermission(permission)){
+
+            }
+        }
+
         return false;
     }
 }
